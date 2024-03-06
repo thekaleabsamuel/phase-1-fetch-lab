859c45c94c2b995390ffe4ea20675eec91381fa5
Kaleab Samuel <thekaleabsamuel@gmail.com>
2024-03-06 09:42:50 -0700

    all test passing

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
+// function fetchBooks(https://anapioficeandfire.com/api/books) {
+// .then(function (response){
+//   console.log(response);
+//   return response.json();
+// })  
   
+// }
+
+function fetchBooks () {
+  return fetch ("https://anapioficeandfire.com/api/books")
+  .then (resp => resp.json())
+  .then(json => renderBooks(json))
 }
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
